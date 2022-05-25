71aa6c479ff533e95ab5eba83d6dde98bd536a07
Jimmychao-al1 <79821770+Jimmychao-al1@users.noreply.github.com>
2022-05-25 22:56:55 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from crawl import AnimeCraw
+
+
+
+if __name__ == '__main__':
+    target = input()
+    kubo , myself ,friday,anigamer,kktv , linetv,grimy = AnimeCraw(target)
+    print(kubo)
+    print(myself)
+    print(friday)
+    print(anigamer)
+    print(kktv)
+    print(linetv)
+    print(grimy)
+    #傳送tkinter介面
+    #如何篩選結果->找出宣傳片,詳細資料
+    pass
