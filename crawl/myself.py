66958008ec7acdff12d1132c57cf8fd6e2feab16
Jimmychao-al1 <79821770+Jimmychao-al1@users.noreply.github.com>
2022-05-31 22:17:11 +0800

    Update myself.py

--- crawl/myself.py
+++ crawl/myself.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 from fuzzywuzzy import fuzz
+import time
+import concurrent.futures
+
 class MyselfSearch:
     def __init__(self,search:str) -> None:
         self.search = search
@@ -17,13 +20,14 @@ class MyselfSearch:
             'srchtxt':f'{search}',
             'searchsubmit' : 'yes'
         }
+        self.time = 0
         pass
     
     def crawlMyself(self):
         self.getRedirectUrl()
         r2 = requests.get(self.redirect,headers=self.headers)
         soup = BeautifulSoup(r2.text,'html.parser')
-        findpage =  soup.find_all("div",{'class':'pg'})
+        findpage =  soup.select("div > div.pgs.cl.mbm > div")
         #getmaxpage
         try:
             self.maxpage = max([int(x.span.text.strip().split()[1]) for x in [x for x in findpage if x.span]])
@@ -32,32 +36,44 @@ class MyselfSearch:
         #crawhomepage
         test = soup.find_all('li',{'class':'pbw'})
         for i in test:
-            tag = i.a.text
-            if (tag not in self.finallst.keys()) and (fuzz.partial_ratio(self.search,tag)>=80):
-                    self.finallst[i.a.text]=self.urlBase+i.a['href']
-        #crawallsubpage
+            tag:str = i.a.text
+            tag = tag[:tag.index('【')]
+            if (tag not in self.finallst.keys()):
+                    self.finallst[tag]=self.urlBase+i.a['href']
+        #crawallsubpage -> multithreading speedup
         if self.maxpage > 1 :
-            self.crawlSubpage()
+            
+            kindex = self.redirect.index('k')
+            urls = [f"{self.redirect[:kindex]}page={page}" for page in range(1, self.maxpage+1)]
+
+            start_time = time.time()
+
+            #同時建立及啟用10個執行緒
+            with concurrent.futures.ThreadPoolExecutor(max_workers=self.maxpage) as executor:
+                executor.map(self.crawlSubpage,urls)
+            end_time = time.time()
+
+            self.time = end_time-start_time
+
         return dict(sorted(self.finallst.items()))
+
+    def crawlSubpage(self,urls):
+        r = requests.get(urls,headers=self.headers)
+        soup = BeautifulSoup(r.text,'html.parser')
+        test = soup.find_all('li',{'class':'pbw'})
+        for i in test:
+            tag:str = i.a.text
+            tag = tag[:tag.index('【')]
+            if (tag not in self.finallst.keys()):
+                    self.finallst[tag]=self.urlBase+i.a['href']
+        pass
 
     def getRedirectUrl(self):
         r = requests.post(self.urlSearch,data=self.my_data,allow_redirects=False)
         self.redirect = self.urlBase + str(r.headers.get('location'))
         pass
 
-    def crawlSubpage(self):
-        kindex = self.redirect.index('k')
-        self.redirect = self.redirect[:kindex]+'page='
-        for pagenum in range(2,self.maxpage+1):
-            self.redirect+=str(pagenum)
-            r = requests.get(self.redirect,headers=self.headers)
-            soup = BeautifulSoup(r.text,'html.parser')
-            test = soup.find_all('li',{'class':'pbw'})
-            for i in test:
-                tag_ = i.a.text
-                if tag_ not in self.finallst.keys() and fuzz.partial_ratio(self.search,tag_)>=80: 
-                    self.finallst[i.a.text]=self.urlBase+i.a['href']
-        pass
+
     def printrst(self) -> str:
         for k,v in sorted(self.finallst.items(),key=lambda x : x[0]):
             print(k+'\t'+v)
@@ -66,3 +82,4 @@ if __name__ == '__main__':
     MyselfAnime = MyselfSearch(input())
     MyselfAnime.crawlMyself()
     MyselfAnime.printrst()
+    print(MyselfAnime.time)
