9eb14bc068799d49b21756e33f1a4caa0fa19fb9
Jimmychao-al1 <79821770+Jimmychao-al1@users.noreply.github.com>
2022-05-31 22:16:38 +0800

    Update kubo.py

--- crawl/kubo.py
+++ crawl/kubo.py
@@ -1,8 +1,6 @@
-from turtle import title
-from fuzzywuzzy import fuzz
 import requests
 from bs4 import BeautifulSoup
-
+import concurrent.futures
 
 class KuboSearch:
     def __init__(self,serarch : str) -> None:
@@ -14,12 +12,14 @@ class KuboSearch:
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36'
         }
+        self.time = 0
         pass
 
     def crwalMain(self):#爬主index
         r = requests.get(self.urlSearch,headers=self.headers)
         soup = BeautifulSoup(r.text,'html.parser')
         container = soup.find('div',{'class':'hl-rb-search hl-marg-right50 clearfix'}) #搜大框
+        #print(container)
 
         test1 = container.find_all('div',{'class':'hl-item-div'}) #爬結果
         if test1:
@@ -32,28 +32,23 @@ class KuboSearch:
                 if '動漫' in movie.find('p',{'class':'hl-item-sub hl-lc-1'}).em.text:
                     tag = movie.find('div',{'class':'hl-item-content'})
                     title ,link= tag.a['title'],self.urlBase+tag.a['href']
-                    if title not in self.finallst.keys() and fuzz.partial_ratio(self.search,title)>=80:
+                    if title not in self.finallst.keys(): 
                         self.finallst[title] = link
                     pass
                 else:
                     pass
         else:
-            return dict(sorted(self.finallst.items()))
+            return self.finallst
 
         if self.maxpage>1:
-            self.serachLoop()
+
+            urls = [f'https://123kubo.tv/search/page/{i}/wd/{self.search}.html' for i in range(1, self.maxpage+1)]
+
+            with concurrent.futures.ThreadPoolExecutor(max_workers=self.maxpage) as executor:
+                executor.map(self.crawlSub,urls)
+            
         return dict(sorted(self.finallst.items()))
-    
-    def serachLoop(self):
-        if self.maxpage>2:
-            for i in range(2,self.maxpage+1):
-                urlSubSearch = f'https://123kubo.tv/search/page/{i}/wd/{self.search}.html'
-                #爬蟲
-                self.crawlSub(urlSubSearch)
-        elif self.maxpage == 2:
-            urlSubSearch = f'https://123kubo.tv/search/page/2/wd/{self.search}.html'
-            self.crawlSub(urlSubSearch)
-            #爬蟲
+
 
     def crawlSub(self,url):#爬分頁
         r = requests.get(url,headers=self.headers)
@@ -65,20 +60,18 @@ class KuboSearch:
                 if '動漫' in movie2.find('p',{'class':'hl-item-sub hl-lc-1'}).em.text:
                     tag = movie2.find('div',{'class':'hl-item-content'})
                     title ,link= tag.a['title'],self.urlBase+tag.a['href']
-                    if title not in self.finallst.keys() and fuzz.partial_ratio(self.search,title)>=80:
+                    if title not in self.finallst.keys():
                         self.finallst[title] = link
                     pass
-                else:
-                    pass
         else:
-            return self.finallst
-
+            pass
+'''
     def printrst(self) -> str:
         for k,v in sorted(self.finallst.items(),key=lambda x : x[0]):
             print(k+'\t'+v)
     def getrst(self):
-        return sorted(self.finallst)
+        return sorted(self.finallst)'''
 if __name__ == '__main__':
     animeSearch = KuboSearch(input())
     animeSearch.crwalMain()
-    animeSearch.printrst()
+    #animeSearch.printrst()
