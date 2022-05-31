69d9efc79eb11287513a05edc5afe64485030e0a
Jimmychao-al1 <79821770+Jimmychao-al1@users.noreply.github.com>
2022-05-31 22:16:10 +0800

    Update grimy.py

--- crawl/grimy.py
+++ crawl/grimy.py
@@ -1,29 +1,54 @@
 ##gimy
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from fuzzywuzzy import fuzz
+import requests
+from bs4 import BeautifulSoup
+import concurrent.futures
+
 class GrimySearch:
-    def __init__(self,search:str,driver) -> None:
+    def __init__(self,search:str) -> None:
         self.search = search
-        self.driver = driver
+        self.urlBase = 'https://gimy.app/'
+        self.urlSearch = f'https://gimy.app/search/-------------.html?wd={search}'
         self.finallst = {}
+        self.maxpage = 0
+
     def GrimyCrawl(self):
-        self.driver.get(f"https://gimy.app/search/-------------.html?wd={self.search}")
-        all_name = self.driver.find_elements(By.TAG_NAME,"a")
-        for name in all_name:
-            title = name.text
-            if self.search in title and fuzz.partial_ratio(self.search,title)>=80:
-                href = name.get_attribute("href")
+        r = requests.get(self.urlSearch)
+        soup = BeautifulSoup(r.text,'html.parser')
+        page = soup.select('#long-page > ul > li.hidden-xs')
+        self.maxpage = max(int(i.text) for i in page)
+        #crawl mainpage
+        lst = soup.find_all('div',{'class':'details-info-min col-md-12 col-sm-12 col-xs-12 clearfix news-box-txt p-0'}) #result list
+        for i in lst:
+            video_type = i.select_one('div.col-md-9.col-sm-8.col-xs-9.clearfix.pb-0 > div > ul > li:nth-child(4) > a').text#類型
+            if video_type == '動漫':
+                target=i.select_one('div.col-md-3.col-sm-4.col-xs-3.news-box-txt-l.clearfix > a')
+                title = target.text
                 if title not in self.finallst.keys():
-                    self.finallst[title] = href
-        self.driver.close()
+                    self.finallst[title] = self.urlBase+target['href']
+
+
+        #crawl subpage
+        if self.maxpage >1:
+            urls = [f'https://gimy.app/search/{self.search}----------{i}---.html' for i in range(2,self.maxpage+1)]
+
+            with concurrent.futures.ThreadPoolExecutor(max_workers=self.maxpage) as executor:
+                executor.map(self.GrimySub,urls)
         return dict(sorted(self.finallst.items()))
 
+    def GrimySub(self,url : str):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text,'html.parser')
+        lst = soup.find_all('div',{'class':'details-info-min col-md-12 col-sm-12 col-xs-12 clearfix news-box-txt p-0'}) #result list
+        for i in lst:
+            video_type = i.select_one('div.col-md-9.col-sm-8.col-xs-9.clearfix.pb-0 > div > ul > li:nth-child(4) > a').text#類型
+            if video_type == '動漫':
+                target=i.select_one('div.col-md-3.col-sm-4.col-xs-3.news-box-txt-l.clearfix > a')
+                title = target.text
+                if title not in self.finallst.keys():
+                    self.finallst[title] = self.urlBase+target['href']
+        pass
+
 if __name__ == '__main__':
-    option = Options()
-    option.add_argument('--headless')
-    driver = webdriver.Chrome('D:\crawler_final\crawl\chromedriver.exe',options=option)
-    a = GrimySearch(input(),driver)
-    a.GrimyCrawl()
+    a = GrimySearch(input())
+    print(a.GrimyCrawl())
     pass
