f8f415975c18198e0ae75a59904d88041802f9e5
Jimmychao-al1 <79821770+Jimmychao-al1@users.noreply.github.com>
2022-06-04 19:02:14 +0800

    Update crawling.py

--- webcrawl/crawling.py
+++ webcrawl/crawling.py
@@ -11,20 +11,20 @@ from fuzzywuzzy import fuzz
 from typing import Dict, List
 class Crawl:
     def __init__(self,search:str) -> None:
-        
-        
         option = Options()
         option.add_argument('--headless')
         self.driver = webdriver.Chrome('D:\crawler_final\webplatform\chromedriver.exe',options=option)
+        
         self.kubo = self.myself = self.friday \
-            = self.kktv = self.linetv = self.grimy = self.anigamer ={}
-        self.hami = {}
+            = self.kktv = self.linetv = self.grimy = self.anigamer =self.hami = {} #放每個平台的爬蟲結果
+        
         self.platformlst = ['kubo','myself','friday','anigamer','grimy']
         self.search = search
-        self.result_dict = {}
+        
+        self.result_dict = {} #放最後含使用者選擇的動畫的平台+連結
 
     
-    def AnimePlatform(self,platform:str):
+    def __AnimePlatform(self,platform:str):
         if platform == 'kubo':
             self.kubo = kubo.KuboSearch(self.search).crwalMain()
         elif platform == 'myself':
@@ -36,23 +36,21 @@ class Crawl:
         elif platform == 'grimy':
             self.grimy = grimy.GrimySearch(self.search).GrimyCrawl()
 
-    def CrawlAll(self):
+    def __CrawlAll(self):
         from concurrent import futures
         import time
         with futures.ThreadPoolExecutor(max_workers=5) as executor:
-            executor.map(self.AnimePlatform,self.platformlst)
+            executor.map(self.__AnimePlatform,self.platformlst)
         self.kktv = kktv.kktvSearch(self.search,self.driver).crawlkktv()
         time.sleep(0.5)
         self.linetv = linetv.LineSearch(self.search,self.driver).LineCraw()
         time.sleep(0.5)
         self.hami = hami.HamivideoSearch(self.search,self.driver).Hamivideocrawl()
 
+    #將各個平台的爬蟲結果(只針對動畫名稱)進行整理 以供使用者選擇準確動畫名稱
     def Outputchoose(self):
-        self.CrawlAll()
-        #anigamer
+        self.__CrawlAll()
         choose_lst = list(self.anigamer.keys())
-        #myself
-        
         for i in self.myself.keys():
             if i not in choose_lst:
                 for j in choose_lst:
@@ -111,10 +109,11 @@ class Crawl:
                 else:
                     choose_lst.append(i)
                 pass
-        #print(sorted(choose_lst))
         return choose_lst 
     
+    #依照使用者選擇的結果爬動畫名稱，以及輸出那些平台內含有使用者選擇的動畫
     def CrawlContent(self,actualsearch:str)->Dict:
+        
         for i in self.anigamer.keys():
             if i == actualsearch:
                 self.result_dict['巴哈姆特動畫瘋'] = self.anigamer[i]
@@ -123,7 +122,6 @@ class Crawl:
             if i == actualsearch:
                 self.result_dict['myself'] = self.myself[i]
                 break
-        #friday
         for i in self.friday.keys():
             if i == actualsearch:
                 self.result_dict['friday'] = self.friday[i]
@@ -153,9 +151,9 @@ class Crawl:
             if i == actualsearch:
                 self.result_dict['hami'] = self.hami[i]
                 break
-        return self.__CrawInform()
-        #回傳結果 -> self.Information & self.CrawInform
+        return self.__CrawInform() #輸出
     
+    #依照結果平台的dictionary 內容決定要爬哪個網站的動畫資訊
     def __CrawInform(self):
         if self.result_dict:
             for i,j in self.result_dict.items():
